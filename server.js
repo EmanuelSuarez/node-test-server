adfd6d472f6208f915d8093aae00871b2ee878b7
EmanuelSuarez <braianesuarez@gmail.com>
2024-01-21 01:57:06 -0300

    updating documents in db

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express()
 
 require('./database')
 
-const trackedFlights = ['AV218', 'AV88', 'AV646']
+const trackedFlights = ['AV88']
 
 setTimeout(() => {
     trackedFlights.forEach(async (flight) => {
         let flightStatus = []
         try {
+            //fetch flight data for flights in "trackedFlights"
             const flightData = await fetch(`https://airlabs.co/api/v9/flight?flight_iata=${flight}&api_key=de700d04-0962-42e8-a7c9-79619c6c90be`)
             const jsonData = await flightData.json()
             const { flight_iata, dep_iata, dep_gate, dep_estimated, dep_actual, dep_time, arr_iata, status } = jsonData.response
@@ -28,8 +29,32 @@ setTimeout(() => {
 
             if (jsonData.response) {
                 const NewFlight = new ExportFlight({flight: flight_iata, origin: dep_iata, gate: dep_gate, etd: dep_estimated, atd: dep_actual, date: dep_time, destination: arr_iata, status: status})
-                await NewFlight.save()
-                console.log(`vuelo guardado: ${NewFlight}`)
+                try {
+                    // look into db to find if the flight already exists
+                    const dbFlight = await db.test.find({flight: flight_iata}, {date: dep_time})
+                    // if exists, update the document
+                    if (dbFlight) {
+                        const { _id } = dbFlight
+                        db.movies.updateOne( { _id: _id },
+                        {
+                        $set: {
+                            gate: NewFlight.gate,
+                            etd: NewFlight.etd,
+                            atd: NewFlight.atd,
+                            status: NewFlight.status
+                        },
+                        $currentDate: { lastUpdated: true }
+                        })
+                        console.log(`flight ${NewFlight.flight} updated on db`);
+                    } else {
+                        // if not, create the document
+                        await NewFlight.save()
+                        console.log(`new flight created: ${NewFlight}`)
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+                
             } else { 
                 console.log(`cant find flight data for ${flight}`);
             }
